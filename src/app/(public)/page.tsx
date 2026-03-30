74732066b9f487d2d9c9d9dc5e80345553ebf0fa
habiburRhaman05 <devhabib2005@gmail.com>
2026-03-30 21:45:08 +0600

    added a kpi reports

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,6 +5,7 @@ import { CareersSection } from '@/components/modules/landing-pages/careers'
 import { FAQSection } from '@/components/modules/landing-pages/faq'
 import { Footer } from '@/components/modules/landing-pages/footer'
 import { HeroSection } from '@/components/modules/landing-pages/hero'
+import KPISReports from '@/components/modules/landing-pages/KpiReport'
 import ProductHighlight from '@/components/modules/landing-pages/ProductHighlight'
 import { ResumeFeaturesSection } from '@/components/modules/landing-pages/resume-features'
 import ScrollToTop from '@/components/modules/landing-pages/ScrollToTop'
@@ -15,9 +16,9 @@ export default function Home() {
   return (
     <main className="w-full bg-white dark:bg-background text-gray-900 dark:text-white">
       <HeroSection />
+      <KPISReports/>
       <TestimonialsSection />
       <ProductHighlight/>
-      <ResumeFeaturesSection />
       <CareersSection />
       <FAQSection/>
       <ScrollToTop/>
